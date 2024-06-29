58de375aa42ebf190d1a83b521d299c89eb85a57
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2024-06-29 22:17:37 +0700

    Add forgot password modal

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useAuth } from "../context/AuthContext"; // Ensure the import path is correct
+import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useAuth();
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,6 +39,14 @@ const Login = () => {
         }
       );
     }
+  };
+
+  const handleForgotPassword = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -77,7 +87,31 @@ const Login = () => {
             Login
           </button>
         </form>
+        <button
+          onClick={handleForgotPassword}
+          className="w-full bg-red-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600"
+        >
+          Quên mật khẩu
+        </button>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm">
+            <h2 className="text-xl font-semibold mb-4">Quên mật khẩu</h2>
+            <p>
+              Xin vui lòng liên lạc số điện thoại tổng đài: 19001900 để lấy lại
+              mật khẩu
+            </p>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            >
+              Đóng
+            </button>
+          </div>
+        </div>
+      )}
       <ToastContainer />
     </section>
   );
